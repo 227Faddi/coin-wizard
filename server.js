4edc6639f6e1a69f151aa5148dfcac421acb9df3
227Faddi <falukhouma27@gmail.com>
2024-09-02 18:43:26 -0400

    es module

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
+import express from 'express';
+import cors from 'cors';
 const app = express();
 const PORT = process.env.PORT || 8000;
-const cors = require('cors')
 
 app.use(cors());
 app.use(express.static('public'))
